6ff97f49a8c6b59d341f64f1d535a408eaa67c35
AKflower <anhkhoabqv@gmail.com>
2024-08-09 10:58:55 +0700

    [Feat]

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -33,7 +33,7 @@ export default {
   data() {
     return {
       night: false,
-      text: ["Front-end Developer", "Web Designer","Mentor"],
+      text: ["Fullstack Developer", "Web Designer","Mentor"],
     };
   },
 };
